4b0993bc94e63da78d6ad0f1456eb4b87bbf3a0a
Julie Kohler <julie.kohler@incontact.com>
2018-04-21 17:36:11 -0600

    Time to merge in master

--- src/foodFactsPackage/headerPanel.java
+++ src/foodFactsPackage/headerPanel.java
@@ -9,53 +9,53 @@ import java.awt.event.ActionListener;
  * Created by julie.kohler on 4/17/2018.
  */
 public class headerPanel {
-    JPanel hP;
-    JButton homeBtn, screen1Btn, screen2Btn;
-    ImageIcon homeIcon;
-    JFrame topFrame;
-    String screen1, screen2;
-
-    headerPanel(String screen1, String screen2) {
-        hP = new JPanel();
-        hP.setOpaque(true);
-        hP.setBackground(new Color(30,144,255));
-        hP.setLayout(new GridLayout(4,1));
-
-//        JFrame topFrame = (JFrame) SwingUtilities.getWindowAncestor(this);
-
-        homeIcon = new ImageIcon("homeIcon.png", "Go to home page");
-        homeBtn = new JButton(homeIcon);
-        homeBtn.setActionCommand("goHome");
-        homeBtn.setOpaque(true);
-        homeBtn.setPreferredSize(new Dimension(40, 40));
-//        homeBtn.addActionListener(new IngredientListener());
-
-        screen1Btn = new JButton(screen1);
-        screen1Btn.setActionCommand(screen1);
-        screen1Btn.setOpaque(true);
-
-        screen2Btn = new JButton(screen2);
-        screen2Btn.setActionCommand(screen2);
-        screen2Btn.setOpaque(true);
-
-        hP.add(homeBtn);
-    }
-
-    public String getScreen1() {
-        return screen1;
-    }
-
-    public String getScreen2() {
-        return screen2;
-    }
-
-    private class screenButtonListener implements ActionListener {
-        public void actionPerformed(ActionEvent e)
-        {
-            String screen1 = headerPanel.getScreen1();
-//          frame.setVisible(false);
-            screen1.setVisible(true);
-
-        }
-    }
+//    JPanel hP;
+//    JButton homeBtn, screen1Btn, screen2Btn;
+//    ImageIcon homeIcon;
+//    JFrame topFrame;
+//    String screen1, screen2;
+//
+//    headerPanel(String screen1, String screen2) {
+//        hP = new JPanel();
+//        hP.setOpaque(true);
+//        hP.setBackground(new Color(30,144,255));
+//        hP.setLayout(new GridLayout(4,1));
+//
+////        JFrame topFrame = (JFrame) SwingUtilities.getWindowAncestor(this);
+//
+//        homeIcon = new ImageIcon("homeIcon.png", "Go to home page");
+//        homeBtn = new JButton(homeIcon);
+//        homeBtn.setActionCommand("goHome");
+//        homeBtn.setOpaque(true);
+//        homeBtn.setPreferredSize(new Dimension(40, 40));
+////        homeBtn.addActionListener(new IngredientListener());
+//
+//        screen1Btn = new JButton(screen1);
+//        screen1Btn.setActionCommand(screen1);
+//        screen1Btn.setOpaque(true);
+//
+//        screen2Btn = new JButton(screen2);
+//        screen2Btn.setActionCommand(screen2);
+//        screen2Btn.setOpaque(true);
+//
+//        hP.add(homeBtn);
+//    }
+//
+//    public String getScreen1() {
+//        return screen1;
+//    }
+//
+//    public String getScreen2() {
+//        return screen2;
+//    }
+//
+//    private class screenButtonListener implements ActionListener {
+//        public void actionPerformed(ActionEvent e)
+//        {
+//            String screen1 = headerPanel.getScreen1();
+////          frame.setVisible(false);
+//            screen1.setVisible(true);
+//
+//        }
+//    }
 }
--- src/foodFactsPackage/recipeDisplayGUI.java
+++ src/foodFactsPackage/recipeDisplayGUI.java
@@ -1,8 +1,16 @@
 package foodFactsPackage;
 
+import javax.swing.*;
+
 /**
  * Created by julie.kohler on 4/16/2018.
  */
 public class recipeDisplayGUI {
+    String recipe;
+    String nutritionFacts;
+
+    JFrame recipeFrame;
+    JPanel recipePanel, nutritionFactsPanel;
+
 
 }
